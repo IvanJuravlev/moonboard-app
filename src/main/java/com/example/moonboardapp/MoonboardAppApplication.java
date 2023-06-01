28e84ddeab8f0869a50c0a300ecf738390adb4e8
MrLeftwing <ivanjuravlev2014@gmail.com>
2023-06-01 09:47:59 +0300

    Initial commit

---
+++ src/main/java/com/example/moonboardapp/MoonboardAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.moonboardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonboardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonboardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moonboardapp/MoonboardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moonboardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonboardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
